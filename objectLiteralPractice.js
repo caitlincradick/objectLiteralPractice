00e048923f1f4ef29c95ee78684f0bcd5d5383ac
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-11 15:47:20 -0800

    Add objectLiteralPractice exercise

---
+++ objectLiteralPractice.js
@@ -0,0 +1,12 @@
+var person = {
+    name: "Caitlin",
+    age: 33, 
+    birthday: "Oct 20",
+    isStudent: true
+}
+console.log(person);
+console.log(person.name);   
+
+person.gender = "female"
+person.wearsGlasses = true
+console.log(Object.keys(person));
